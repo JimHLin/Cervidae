2278b4589cc19f496b97561e3390ff000f0d108b
Jim Lin <jhlin5083@gmail.com>
2025-02-14 16:02:22 +0800

    feat: db migrations

---
+++ justfile
@@ -0,0 +1,18 @@
+PROJECT := "cervidae"
+REPO := "https://github.com" / "JimHLin" / PROJECT
+ROOT_DIR := justfile_directory()
+OUTPUT_DIR := ROOT_DIR / "target"
+
+migrate:
+    cargo run
+
+export COMPOSE_PROJECT_NAME := "cervidae"
+
+local:
+    docker compose up -d
+
+local-min:
+    docker compose up -d deer_postgres deer_adminer
+
+local-down:
+    docker compose down -v
---
+++ migrations/20250214000000_init.sql
@@ -0,0 +1,60 @@
+CREATE TABLE Cervidae (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    image_url VARCHAR(255),
+    kill_count INTEGER DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by UUID NOT NULL,
+    updated_by UUID NOT NULL,
+    FOREIGN KEY (created_by) REFERENCES User(id),
+    FOREIGN KEY (updated_by) REFERENCES User(id)
+);
+
+CREATE TABLE User (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE Review (
+    user_id UUID NOT NULL,
+    cervidae_id UUID NOT NULL,
+    danger_level INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, cervidae_id),
+    FOREIGN KEY (user_id) REFERENCES User(id),
+    FOREIGN KEY (cervidae_id) REFERENCES Cervidae(id)
+);
+
+CREATE TABLE Comment (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    content TEXT NOT NULL,
+    parent_id UUID,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES User(id),
+    FOREIGN KEY (parent_id) REFERENCES Comment(id)
+);
+
+CREATE TABLE Crime (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE Crime_Cervidae (
+    crime_id UUID NOT NULL,
+    cervidae_id UUID NOT NULL,
+    PRIMARY KEY (crime_id, cervidae_id),
+    FOREIGN KEY (crime_id) REFERENCES Crime(id),
+    FOREIGN KEY (cervidae_id) REFERENCES Cervidae(id)
+);
