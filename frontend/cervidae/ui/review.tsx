23f708aed16252ba82af9c370cc8a8d50136fbfc
Jim Lin <jhlin5083@gmail.com>
2025-03-05 15:25:10 +0800

    feat: review component added

---
+++ frontend/cervidae/ui/review.tsx
@@ -0,0 +1,24 @@
+import DangerRating from "./danger_rating";
+
+export default function Review(props: {review: any}){
+    return (
+        <div className="flex flex-col w-ful bg-orange-900 p-4 gap-4">
+            <h2 className="text-2xl font-bold">{props.review.title}</h2>
+            <div className="flex">
+                <DangerRating />
+            </div>
+            <p className="text-sm text-gray-50">
+                {props.review.body.slice(0, 100)}
+                {props.review.body.length > 100 && '...'}
+            </p>
+            {props.review.body.length > 100 &&
+            <p className="text-xs text-gray-50 text-right">
+                <a href={`/deer/${props.review.cervidaeId}`}>Read More</a>
+            </p>
+            }
+            <p className="text-xs text-gray-50 text-right">
+                - {props.review.user.name}
+            </p>
+        </div>
+    )
+}
