9f59ab8161909776162230fffe91fc06bd5ae43a
Jim Lin <jhlin5083@gmail.com>
2025-03-06 17:02:25 +0800

    feat: Made login page, gave up on NextAuth.js (super confusing versioning and docs)

--- frontend/cervidae/app/layout.tsx
+++ frontend/cervidae/app/layout.tsx
@@ -11,16 +11,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="min-h-screen flex flex-col justify-between">
         <div className="flex flex-col items-center justify-center h-10 bg-green-800 fixed w-full z-50">
           <Link href="/" className="text-white font-serif text-2xl tracking-wider">Cervidae</Link>
         </div>
+        <div className="pt-10">
         <ClientProvider>
           {children}
         </ClientProvider>
+        </div>
         <div className="flex flex-row gap-4 h-10 bg-green-800 w-full mt-10">
         This is a footer
-        </div>
         </div>
       </body>
     </html>
