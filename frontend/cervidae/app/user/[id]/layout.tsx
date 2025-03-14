ec6d98dc904dd66c6ab87698848fa60fd975276c
Jim Lin <jhlin5083@gmail.com>
2025-03-14 18:19:06 +0800

    feat: updated user page  to separate files with header layout

--- frontend/cervidae/app/user/[id]/layout.tsx
+++ frontend/cervidae/app/user/[id]/layout.tsx
@@ -1,14 +1,41 @@
 'use client'
 import {useState} from 'react'
+import { usePathname, redirect } from 'next/navigation'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    const [active, setActive] = useState(0);
+    const pathname = usePathname();
+    const [active, setActive] = useState(() => {
+        switch (pathname.substring(pathname.lastIndexOf('/') + 1)) {
+            case 'profile':
+                console.log('profile');
+                return 0;
+            case 'reviews':
+                console.log('reviews');
+                return 1;
+            case 'comments':
+                console.log('comments');
+                return 2;
+            default:
+                return 0;
+        }
+    });
+    const switchtab = (tab: number) => {
+        setActive(tab);
+        switch (tab) {
+            case 0:
+                redirect(pathname.substring(0, pathname.lastIndexOf('/')) + '/profile');
+            case 1:
+                redirect(pathname.substring(0, pathname.lastIndexOf('/')) + '/reviews');
+            case 2:
+                redirect(pathname.substring(0, pathname.lastIndexOf('/')) + '/comments');
+        }
+    }
     return (
         <div>
             <div className="flex flex-row w-full justify-evenly bg-green-900 shadow-inner shadow-black flex-1">
-                <button onClick={() => setActive(0)} className={`flex-1 p-1  ${active === 0 ? 'bg-green-950 shadow-inner shadow-black' : 'hover:bg-green-800 hover:shadow-inner hover:shadow-black'}`}>Profile</button>
-                <button onClick={() => setActive(1)} className={`flex-1 p-1  ${active === 1 ? 'bg-green-950 shadow-inner shadow-black' : 'hover:bg-green-800 hover:shadow-inner hover:shadow-black'}`}>Reviews</button>
-                <button onClick={() => setActive(2)} className={`flex-1 p-1  ${active === 2 ? 'bg-green-950 shadow-inner shadow-black' : 'hover:bg-green-800 hover:shadow-inner hover:shadow-black'}`}>Comments</button>
+                <button onClick={() => {switchtab(0)}} className={`flex-1 p-1  ${active === 0 ? 'bg-green-950 shadow-inner shadow-black' : 'hover:bg-green-800 hover:shadow-inner hover:shadow-black'}`}>Profile</button>
+                <button onClick={() => {switchtab(1)}} className={`flex-1 p-1  ${active === 1 ? 'bg-green-950 shadow-inner shadow-black' : 'hover:bg-green-800 hover:shadow-inner hover:shadow-black'}`}>Reviews</button>
+                <button onClick={() => {switchtab(2)}} className={`flex-1 p-1  ${active === 2 ? 'bg-green-950 shadow-inner shadow-black' : 'hover:bg-green-800 hover:shadow-inner hover:shadow-black'}`}>Comments</button>
             </div>
             {children}
         </div>
---
+++ frontend/cervidae/app/user/[id]/profile/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+import { useParams } from "next/navigation";
+import { useQuery } from "urql";
+import edit  from "@/public/edit.svg"
+
+const UserQuery = `
+    query User($id: String!) {
+        user(id: $id) {
+            id
+            name
+            email
+            createdAt
+            updatedAt
+        }
+    }
+`;
+export default function Page() {
+    const { id } = useParams();
+    const [result, reexecuteQuery] = useQuery({
+        query: UserQuery,
+        variables: { id },
+    });
+    return (
+        <div className="mt-5">
+            <div className="flex justify-end">
+                <img src={edit.src} alt="edit" className="w-7 h-7 invert mr-4 cursor-pointer hover:bg-gray-400 rounded-md p-1" />
+            </div>
+            <div className="grid grid-cols-2 gap-4 p-4  w-10/12 mx-auto">
+                <label htmlFor="name">Name: </label>
+                <h1>{result.data?.user.name}</h1>
+                <label htmlFor="email">Email: </label>
+                <p>{result.data?.user.email}</p>
+                <label>Created At: </label>
+                <p>{result.data?.user.createdAt}</p>
+                <label>Updated At:</label>
+                <p>{result.data?.user.updatedAt}</p>
+            </div>
+            <div className="flex justify-center">
+                <button className="bg-green-700 text-white px-4 py-2 rounded-md mt-4">Click here to reset password</button>
+            </div>
+        </div>
+    )
+}
