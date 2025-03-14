17a671caf8d4bfe8566a2982a79cc928d3333ebb
Jim Lin <jhlin5083@gmail.com>
2025-03-14 18:37:07 +0800

    feat: updated tempalte page for user comments and reviews

--- frontend/cervidae/app/user/[id]/comments/page.tsx
+++ frontend/cervidae/app/user/[id]/comments/page.tsx
@@ -1,7 +1,38 @@
+'use client'
+import {useQuery} from 'urql'
+import { useParams } from 'next/navigation'
+import Comment from '@/ui/comment'
+const commentsQueryString = `
+    query Comments($id: ID!) {
+        userComments(id: $id) {
+            id
+            parent{
+                id
+                content
+            }
+            content
+            createdAt
+            updatedAt
+            user{
+                id
+                name
+            }
+        }
+    }
+`
 export default function Comments() {
+    const params = useParams();
+    const id = params.id;
+    const [result, reexecuteQuery] = useQuery({
+        query: commentsQueryString,
+        variables: {id}
+    })
     return (
         <div>
             <h1>Comments</h1>
+            {result.data?.userComments.map((comment: any) => (
+                <Comment key={comment.id} comment={comment} reload={() => {}} />
+            ))}
         </div>
     )
 }
--- frontend/cervidae/app/user/[id]/reviews/page.tsx
+++ frontend/cervidae/app/user/[id]/reviews/page.tsx
@@ -1,7 +1,39 @@
+'use client'
+import {useQuery} from 'urql'
+import { useParams } from 'next/navigation'
+import Review from '@/ui/review'
+const reviewsQueryString = `
+    query Reviews($id: ID!) {
+        userReviews(id: $id) {
+            deer{
+                id
+                name
+            }
+            user{
+                id
+                name
+            }
+            dangerLevel
+            title
+            body
+            createdAt
+            updatedAt
+        }
+    }
+`
 export default function Reviews() {
+    const params = useParams();
+    const id = params.id;
+    const [result, reexecuteQuery] = useQuery({
+        query: reviewsQueryString,
+        variables: {id}
+    })
     return (
         <div>
             <h1>Reviews</h1>
+            {result.data?.userReviews.map((review: any) => (
+                <Review key={review.deer.id} review={review} deerId={review.deer.id} reload={() => {}} editReview={() => {}} />
+            ))}
         </div>
     )
 }
