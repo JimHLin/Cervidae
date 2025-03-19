adcf7528537679c07674e2bed65c88b04800d60f
Jim Lin <jhlin5083@gmail.com>
2025-03-19 13:13:02 +0800

    feat: User profile page compelted

--- frontend/cervidae/ui/reset-password.tsx
+++ frontend/cervidae/ui/reset-password.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import PasswordInput from './password-input'
 export default function ResetPassword(props: {setShow: (show: boolean) => void, resetPassword: (input: any) => any, id: string}) {
     const [password, setPassword] = useState("");
     const [newPassword, setNewPassword] = useState("");
@@ -30,12 +30,12 @@ export default function ResetPassword(props: {setShow: (show: boolean) => void, 
         <div className="fixed w-screen flex justify-center items-center" onClick={() => {props.setShow(false);}}>
             <div className=" z-40 rounded-md bg-gray-800 p-4" onClick={(e) => e.stopPropagation()}>
                 <form className="flex flex-col gap-2">
-                    <label htmlFor="title">Current Ppassword</label>
-                    <input autoComplete="none" className="w-full border-2 dark:border-gray-300 dark:bg-gray-900 rounded-md p-2" autoFocus name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <label htmlFor="title">Current Password</label>
+                    <PasswordInput value={password} onChange={(value) => setPassword(value)} autoFocus={true} />
                     <label htmlFor="newPassword">New Password</label>
-                    <input autoComplete="none" className="w-full border-2 dark:border-gray-300 dark:bg-gray-900 rounded-md p-2" name="newPassword" value={newPassword} onChange={(e) => setNewPassword(e.target.value)}/>
+                    <PasswordInput value={newPassword} onChange={(value) => setNewPassword(value)} />
                     <label htmlFor="confirmPassword">Confirm New Password</label>
-                    <input autoComplete="none" className="w-full border-2 dark:border-gray-300 dark:bg-gray-900 rounded-md p-2" type="password" name="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></input>
+                    <PasswordInput value={confirmPassword} onChange={(value) => setConfirmPassword(value)} />
                 {error && <p>{error}</p>}
                 {!matching && <p className="text-red-400 text-sm">Passwords do not match</p>}
                 <button type="button" onClick={submit}>Submit</button>
