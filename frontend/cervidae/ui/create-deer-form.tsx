1681cf1e18617bfc069d0b9adc713c1ef1384500
Jim Lin <jhlin5083@gmail.com>
2025-03-21 18:31:15 +0800

    feat: createDeer template added to form

--- frontend/cervidae/ui/create-deer-form.tsx
+++ frontend/cervidae/ui/create-deer-form.tsx
@@ -2,15 +2,33 @@
 import { useState } from "react";
 import Tiptap from "./tiptap";
 import Test from "./test";
+import { useAuth } from "@/ui/auth-provider";
+import { useMutation, gql } from "urql";
 
 export default function CreateDeerForm() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [killCount, setKillCount] = useState(0);
-
+  const { userId, isAdmin } = useAuth();
+  const [createDeer, executeCreateDeer] = useMutation(gql`
+    mutation CreateDeer($input: CreateDeerInput!) {
+      createDeer(input: $input) {
+        id
+      }
+    }
+  `);
+  
   const handleCreateDeer = () => {
-    console.log(name, description, imageUrl, killCount);
+    const input = executeCreateDeer({
+        input: {
+            userId: userId,
+            name: "testDeer",
+            description: "testDescription",
+            killCount: 50
+        }
+    })
+    console.log(input)
   };
 
   return (
