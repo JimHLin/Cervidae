fb8ce1b7c2b4bb8737a9edb05d12143033bb4374
Jim Lin <jhlin5083@gmail.com>
2025-03-27 18:10:25 +0800

    feat: added ability to upload and preview image files

--- frontend/cervidae/ui/create-deer-form.tsx
+++ frontend/cervidae/ui/create-deer-form.tsx
@@ -6,40 +6,98 @@ import GenericInput from "./generic-input";
 import { useAuth } from "@/ui/auth-provider";
 import { useMutation, gql } from "urql";
 import { useRouter } from "next/navigation";
+import striptags from "striptags";
+const createDeerMutation = gql`
+mutation CreateDeer($input: CreateDeerInput!) {
+  createDeer(input: $input) {
+    id
+    name
+    description
+    imageUrl
+    killCount
+  }
+}
+`;
+
+const getUploadUrlMutation = gql`
+mutation GetUploadUrl($contentType: String!) {
+  getUploadUrl(contentType: $contentType)
+}
+`;
 
 export default function CreateDeerForm() {
   const [name, setName] = useState("");
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [file, setFile] = useState<{file: File, src: string} | null>(null);
   const [killCount, setKillCount] = useState(0);
   const router = useRouter();
   const { userId, isAdmin } = useAuth();
   const [editorContent, setEditorContent] = useState("");
-  const [createDeer, executeCreateDeer] = useMutation(gql`
-    mutation CreateDeer($input: CreateDeerInput!) {
-      createDeer(input: $input) {
-        id
-        name
-        description
-        imageUrl
-        killCount
-      }
-    }
-  `);
+  const [createDeer, executeCreateDeer] = useMutation(createDeerMutation);
+  const [getUploadUrl, executeGetUploadUrl] = useMutation(getUploadUrlMutation);
   
   const handleCreateDeer = async () => {
-    const input = await executeCreateDeer({
-        input: {
-            userId: userId,
-            name: name,
-            description: editorContent,
-            killCount: killCount,
-            imageUrl: imageUrl
-        }
-    })
-    if (input.error) {
-      console.error(input.error);
-    }else{
-      router.push('/');
+    let uploadedImageUrl = null;
+    if(striptags(editorContent).length === 0) {
+      alert("Please enter a description");
+      return;
+    }
+    if (file) {
+      let res = await executeGetUploadUrl({
+        contentType: file.file.type
+      });
+  
+      if (res.data?.getUploadUrl) {
+        uploadedImageUrl = await uploadToS3(file.file, res.data.getUploadUrl);
+        console.log("Uploaded URL:", uploadedImageUrl);
+      } else {
+        console.error("Upload failed", res.error);
+        return;
+      }
+    }
+  
+    const input = {
+      userId: userId,
+      name: name,
+      description: editorContent,
+      killCount: killCount,
+      imageUrl: uploadedImageUrl,
+    };
+  
+    console.log("Payload:", input);
+  
+    let createRes = await executeCreateDeer({input: input});
+    if (createRes.error) {
+      console.error("Mutation failed:", createRes.error);
+    } else {
+      //router.push('/');
+    }
+  };
+  
+
+  const uploadToS3 = async (file: File, uploadUrl: string) => {
+    const response = await fetch(uploadUrl, {
+      method: "PUT",
+      body: file,
+      headers: {
+        "Content-Type": file.type,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Upload failed");
+    }
+    console.log(uploadUrl.split("?")[0]);
+    return uploadUrl.split("?")[0]; // Get the file URL without query params
+  };
+  
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setFile({file: file, src: e.target?.result as string});
+      };
+      reader.readAsDataURL(file);
     }
   };
 
@@ -50,7 +108,8 @@ export default function CreateDeerForm() {
             <GenericInput value={name} onChange={(value) => setName(value)} />
         </div>
       <div className="flex justify-center items-center w-full h-40 object-scale-down bg-green-900">
-        <input type="file" onChange={(e) => setImageUrl(e.target.value)} />
+        <input type="file" onChange={handleFileChange} accept="image/*"/>
+        <img width={50} height={50} src={file?.src ?? "https://i.postimg.cc/L69Q7Xzf/defaultdeer.webp"} alt="Deer" />
       </div>
       <div className="flex flex-row gap-2 items-center">
         <div className="min-w-16 flex justify-center items-center">
